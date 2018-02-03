a41143aaccb7bca832fdb975b9f0330565bb2da9
Clément Lesaege <clemage@live.fr>
2018-02-03 21:30:27 +0000

    Change to permitted

--- contracts/Blacklist.sol
+++ contracts/Blacklist.sol
@@ -7,26 +7,29 @@ contract Blacklist is BlacklistInterface, Ownable {
 
     mapping (bytes32 => bool) blacklisted;
     
-    /** @dev Add a node to the blacklist.
-     *  @param node The node to add to the blacklist.
+    /**
+     * @dev Add a node to the blacklist.
+     * @param node The node to add to the blacklist.
      */
     function blacklist(bytes32 node) public onlyOwner {
         blacklisted[node] = true;
         Blacklisted(node);
     }
     
-    /** @dev Remove a node from the blacklist.
-     *  @param node The node to remove from the blacklist.
+    /** 
+     * @dev Remove a node from the blacklist.
+     * @param node The node to remove from the blacklist.
      */
     function unblacklist(bytes32 node) public onlyOwner {
         blacklisted[node] = false;
         Unblacklisted(node);
     }
     
-    /** @dev Return true if the node is allowed, false otherwise. Every nodes, except the blacklisted ones, are allowed.
+    /**
+     *  @dev Return true if the node is permitted, false otherwise. Every nodes, except the blacklisted ones, are permitted.
      *  @param node The node.
      */
-    function isAllowed(bytes32 node) public view returns (bool) {
+    function isPermitted(bytes32 node) public view returns (bool) {
         return !blacklisted[node];
     }
 }
--- contracts/BlacklistInterface.sol
+++ contracts/BlacklistInterface.sol
@@ -5,19 +5,8 @@ interface BlacklistInterface {
     event Blacklisted(bytes32 indexed node);
     event Unblacklisted(bytes32 indexed node);
     
-    /** @dev Add a node to the blacklist.
-     *  @param node The node to add to the blacklist.
-     */
     function blacklist(bytes32 node) public;
-    
-    /** @dev Remove a node from the blacklist.
-     *  @param node The node to remove from the blacklist.
-     */
     function unblacklist(bytes32 node) public;
-    
-    /** @dev Return true if the node is allowed, false otherwise. Every nodes, except the blacklisted ones, are allowed.
-     *  @param node The node.
-     */
     function isAllowed(bytes32 node) public view returns (bool);
 
 }
