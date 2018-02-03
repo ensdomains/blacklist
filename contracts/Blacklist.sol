5799d7f36dba83d53c7b0639337c8370f3908a0c
Clément Lesaege <clemage@live.fr>
2018-02-03 20:56:25 +0000

    Use permission standard.

--- contracts/Blacklist.sol
+++ contracts/Blacklist.sol
@@ -6,18 +6,27 @@ import "./Ownable.sol";
 contract Blacklist is BlacklistInterface, Ownable {
 
     mapping (bytes32 => bool) blacklisted;
-
+    
+    /** @dev Add a node to the blacklist.
+     *  @param node The node to add to the blacklist.
+     */
     function blacklist(bytes32 node) public onlyOwner {
         blacklisted[node] = true;
         Blacklisted(node);
     }
-
+    
+    /** @dev Remove a node from the blacklist.
+     *  @param node The node to remove from the blacklist.
+     */
     function unblacklist(bytes32 node) public onlyOwner {
         blacklisted[node] = false;
         Unblacklisted(node);
     }
-
-    function isBlacklisted(bytes32 node) public view returns (bool) {
-        return blacklisted[node];
+    
+    /** @dev Return true if the node is allowed, false otherwise. Every nodes, except the blacklisted ones, are allowed.
+     *  @param node The node.
+     */
+    function isAllowed(bytes32 node) public view returns (bool) {
+        return !blacklisted[node];
     }
 }
